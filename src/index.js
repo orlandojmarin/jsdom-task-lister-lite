4887464b8ccee3b040dc5ab776855c5b87c85b6e
Orlando Marin <orlandojermainemarin@gmail.com>
2021-02-24 17:30:16 -0800

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (event) => {
+  //your code here
 });
+ 
+
+const form = document.querySelector("#create-task-form")
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const li = document.createElement("li")
+
+  li.innerText = e.target["new-task-description"].value
+
+  const ul = document.querySelector("#tasks")
+
+  ul.appendChild(li)
+
+  const button = document.createElement("button");
+  button.innerText = "x";
+
+  li.appendChild(button);
+
+  button.addEventListener("click", function(e) {
+    e.target.parentElement.remove();
+    })
+  })
